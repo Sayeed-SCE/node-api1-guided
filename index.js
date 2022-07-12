febb95dca2bffc316ce7147acbac384d833a3cea
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-07-12 14:01:44 -0700

    wip

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // import the server and start it!
-console.log('hello world!')
+const express = require ('express')
+
+const server = express ()
+
+server.get('/hello', (req, res) =>{
+    res.json({message: 'hello', })
+})
+
+server.listen(5000, () => {
+    console.log('server is listening on 5000')
+
+})
