2cbb51755f3ee852c869f9df59cfc6f548cfb612
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-07-12 18:10:24 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,28 +1,53 @@
-const express = require ("express");
+const express = require("express");
+const Dog = require("./dog-model");
 // IMPORTS AT THE TOP
 // IMPORTS AT THE TOP
 // IMPORTS AT THE TOP
 // IMPORTS AT THE TOP
 
-
 // INSTANCE OF EXPRESS APP
 // INSTANCE OF EXPRESS APP
 // INSTANCE OF EXPRESS APP
 // INSTANCE OF EXPRESS APP
-const server = express ()
+const server = express();
 
 // GLOBAL MIDDLEWARE
+server.use(express.json());
 
 // ENDPOINTS
 // ENDPOINTS
 // ENDPOINTS
 // ENDPOINTS
-server.get('/hello', (req, res) => {
-    res.json({message: 'hello' })
-})
-
+server.get("/hello", (req, res) => {
+  res.json({ message: "hello" });
+});
 
 // [GET]    /             (Hello World endpoint)
+server.get("/api/dogs", (req, res) => {
+  //pull dogs from DB
+  Dog.findAll()
+    .then((dogs) => {
+      //send dogs back to client
+      res.json(dogs);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "something bad happened",
+        error: err.message,
+      });
+    });
+});
+
+server.get("/api/dogs", async (req, res) => {
+  try {
+    const dogs = await Dog.findAll();
+    res.json(dogs);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "something bad happened", error: err.message });
+  }
+});
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
@@ -34,9 +59,4 @@ server.get('/hello', (req, res) => {
 // EXPOSING THE SERVER TO OTHER MODULES
 // EXPOSING THE SERVER TO OTHER MODULES
 // EXPOSING THE SERVER TO OTHER MODULES
-module.exports = server
-
-
-
-
-
+module.exports = server;
